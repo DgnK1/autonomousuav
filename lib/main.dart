8bdccc786649015573943ea29584ad64a072936b
DgnK1 <133643470+DgnK1@users.noreply.github.com>
2025-09-14 18:26:49 +0800

    autonomousuav

---
+++ lib/main.dart
@@ -0,0 +1,563 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login 2025',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  LoginPageState createState() => LoginPageState();
+}
+
+class LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'Autonomous UAV',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 32,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+              SizedBox(height: 64),
+
+              Text(
+                'Create an account',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+              SizedBox(height: 6),
+
+              Text(
+                'Enter your email to sign up for this app',
+                style: TextStyle(
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.normal,
+                  fontSize: 18,
+                  color: Color(0xFF1C1C1C),
+                ),
+              ),
+              SizedBox(height: 26),
+
+              TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                  labelText: 'Username',
+                  hintText: 'email@domain.com',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: 16),
+
+              /*TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              SizedBox(height: 26),*/
+              SizedBox(
+                width: double.infinity,
+                height: 49,
+                child: ElevatedButton(
+                  onPressed: () {
+                    // pseudo login action
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ControlsTabPage(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  child: Text(
+                    'Continue',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 26),
+              Row(
+                children: [
+                  Expanded(child: Divider(color: Colors.grey, thickness: 1)),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      'or',
+                      style: TextStyle(
+                        fontFamily: 'Roboto',
+                        fontSize: 14,
+                        color: Color(0xFF87879D),
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Divider(color: Colors.grey, thickness: 1)),
+                ],
+              ),
+              SizedBox(height: 26),
+
+              SizedBox(
+                width: double.infinity,
+                height: 49,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    //pseudo Google login
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ControlsTabPage(),
+                      ),
+                    );
+                  },
+                  icon: Icon(
+                    Icons.g_mobiledata,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                  ),
+                  label: Text(
+                    'Continue with Google',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(239, 239, 239, 239),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 16),
+
+              SizedBox(
+                width: double.infinity,
+                height: 49,
+                child: ElevatedButton.icon(
+                  onPressed: () {
+                    // Handle Apple login
+                  },
+                  icon: Icon(
+                    Icons.apple,
+                    color: const Color.fromARGB(255, 0, 0, 0),
+                    size: 24,
+                  ),
+                  label: Text(
+                    'Continue with Apple',
+                    style: TextStyle(
+                      color: const Color.fromARGB(255, 0, 0, 0),
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(239, 239, 239, 239),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 32),
+
+              Center(
+                child: Text(
+                  'Forgot Password?',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 14, color: Color(0xFF87879D)),
+                ),
+              ),
+              SizedBox(height: 10),
+
+              Center(
+                child: Text(
+                  "Don't have an account? Sign Up",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontFamily: 'Roboto',
+                    fontSize: 14,
+                    color: Color(0xFF87879D),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ControlsTabPage extends StatefulWidget {
+  const ControlsTabPage({super.key});
+
+  @override
+  State<ControlsTabPage> createState() => _ControlsTabPageState();
+}
+
+// Update the ControlsTabPage class to include the ActivityPage
+class _ControlsTabPageState extends State<ControlsTabPage> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    const SearchBarApp(), // Home Page
+    const ActivityPage(), // new ActivityPage
+    const ManualControlsPage(), // Manual Controls Page
+    const SettingsPage(), // Settings Page
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex], // Display the selected page
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: (int index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'HOME'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.show_chart_rounded),
+            label: 'ACTIVITY',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.map), label: 'MANUAL'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'SETTINGS',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Move the SearchBarApp class outside of ControlsTabPage
+class SearchBarApp extends StatefulWidget {
+  const SearchBarApp({super.key});
+
+  @override
+  State<SearchBarApp> createState() => _SearchBarAppState();
+}
+
+class _SearchBarAppState extends State<SearchBarApp> {
+  bool isDark = false;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData themeData = ThemeData(
+      brightness: isDark ? Brightness.dark : Brightness.light,
+    );
+
+    return MaterialApp(
+      theme: themeData,
+      home: Scaffold(
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              SearchAnchor(
+                builder: (BuildContext context, SearchController controller) {
+                  return SearchBar(
+                    controller: controller,
+                    padding: const WidgetStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0),
+                    ),
+                    onTap: () {
+                      controller.openView();
+                    },
+                    onChanged: (_) {
+                      controller.openView();
+                    },
+                    leading: const Icon(Icons.search),
+                    trailing: <Widget>[
+                      Tooltip(
+                        message: 'Change brightness mode',
+                        child: IconButton(
+                          isSelected: isDark,
+                          onPressed: () {
+                            setState(() {
+                              isDark = !isDark;
+                            });
+                          },
+                          icon: const Icon(Icons.wb_sunny_outlined),
+                          selectedIcon: const Icon(Icons.brightness_2_outlined),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+                suggestionsBuilder:
+                    (BuildContext context, SearchController controller) {
+                      return List<ListTile>.generate(5, (int index) {
+                        final String item = 'item $index';
+                        return ListTile(
+                          title: Text(item),
+                          onTap: () {
+                            setState(() {
+                              // Add your code here
+                            });
+                          },
+                        );
+                      });
+                    },
+              ),
+              SizedBox(height: 24),
+              // Add some space between the search bar and the status text
+              Text(
+                'Status',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.water_drop,
+                            size: 40,
+                            color: Color.fromARGB(255, 41, 41, 49),
+                          ),
+                          Text(
+                            '60%',
+                            style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Color(0xFF1C1C1C),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 48),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.thermostat,
+                            size: 40,
+                            color: Color.fromARGB(255, 255, 0, 0),
+                          ),
+                          Text(
+                            '25°C',
+                            style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Color(0xFF1C1C1C),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Spacer(), // Take up the remaining space
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.battery_6_bar, size: 40),
+                          Text(
+                            '85%',
+                            style: TextStyle(
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.bold,
+                              fontSize: 24,
+                              color: Color(0xFF1C1C1C),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 48),
+                ],
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  // pseudo login action
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ControlsTabPage()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                  minimumSize: Size(0, 0),
+                  padding: EdgeInsets.symmetric(horizontal: 48, vertical: 12),
+                ),
+                child: Text(
+                  'START',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              SizedBox(height: 26),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ActivityPage extends StatelessWidget {
+  const ActivityPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Activity', style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        automaticallyImplyLeading: false,
+      ),
+      body: const Center(
+        child: Text(
+          'Activity Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class ManualControlsPage extends StatefulWidget {
+  const ManualControlsPage({super.key});
+
+  @override
+  State<ManualControlsPage> createState() => _ManualControlsPageState();
+}
+
+class _ManualControlsPageState extends State<ManualControlsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Manual Controls',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        automaticallyImplyLeading: false,
+      ),
+      body: const Center(
+        child: Text(
+          'Manual Controls Page',
+          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({super.key});
+
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Settings', style: TextStyle(color: Colors.white)),
+        backgroundColor: const Color.fromARGB(255, 0, 1, 2),
+        automaticallyImplyLeading: false,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                // Navigate back to the login page
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()),
+                  (route) => false, // Remove all previous routes
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+              ),
+              child: const Text(
+                'Exit',
+                style: TextStyle(fontSize: 16, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
